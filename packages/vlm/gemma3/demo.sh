f61bf9763ac4d91b7812aee30159fd16913ca482
Siddhartha (Sid) <124874523+sids-amd@users.noreply.github.com>
2025-10-09 07:27:35 +0800

    Gemma3-4B package with test and demo scripts on llamacpp backend (#17)
    * Gemma3-4B package added and verified to work with the llamacpp backend on STX-H
    
    * Added missing demo.sh script to Gemma3 package

---
+++ packages/vlm/gemma3/demo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+# SPDX-License-Identifier: MIT
+
+export PATH=/ryzers/llamacpp/build/bin/:$PATH
+llama-server -hf ggml-org/gemma-3-4b-it-GGUF
