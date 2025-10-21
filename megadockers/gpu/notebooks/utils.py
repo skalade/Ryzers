c5e488c2e39b3f984fde5cac4e19a656e3618614
skalade <sarunas.kalade@amd.com>
2025-10-21 17:53:21 -0600

    finalize notebook 3

--- megadockers/gpu/notebooks/utils.py
+++ megadockers/gpu/notebooks/utils.py
@@ -1,4 +1,6 @@
 import torch
+import matplotlib.pyplot as plt
+import cv2
 
 @torch.no_grad()
 def sanitize_norm(policy):
@@ -23,3 +25,12 @@ def sanitize_norm(policy):
     s = torch.where(s.abs() < 1e-6, torch.ones_like(s), s)
     policy.normalize_inputs.buffer_observation_state.mean = torch.nn.Parameter(m, requires_grad=False)
     policy.normalize_inputs.buffer_observation_state.std  = torch.nn.Parameter(s, requires_grad=False)
+
+
+def plot_image(image_path: str):
+    """Image plotting helper"""
+    img = cv2.imread(image_path)
+    fig, ax = plt.subplots(figsize=(4, 4))
+    ax.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
+    ax.xaxis.set_visible(False)
+    ax.yaxis.set_visible(False)
