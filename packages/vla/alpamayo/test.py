05df61492d19cfc4577c68234226ed12025977fb
skalade <sarunas.kalade@amd.com>
2026-01-20 17:29:34 -0700

    update test with warmup before inference

--- packages/vla/alpamayo/test.py
+++ packages/vla/alpamayo/test.py
@@ -2,27 +2,6 @@
 # Copyright(C) 2025 Advanced Micro Devices, Inc. All rights reserved.
 # SPDX-License-Identifier: MIT
 
-import copy
-import numpy as np
-import mediapy as mp
-import pandas as pd
-
-import torch
-from alpamayo_r1.models.alpamayo_r1 import AlpamayoR1
-from alpamayo_r1.load_physical_aiavdataset import load_physical_aiavdataset
-from alpamayo_r1 import helper
-
-import time
-
-model = AlpamayoR1.from_pretrained("nvidia/Alpamayo-R1-10B", dtype=torch.bfloat16).to("cuda")
-processor = helper.get_processor(model.tokenizer)
-
-clip_ids = pd.read_parquet("clip_ids.parquet")["clip_id"].tolist()
-clip_id = clip_ids[774]
-# clip_id = '030c760c-ae38-49aa-9ad8-f5650a545d26'
-
-data = load_physical_aiavdataset(clip_id)
-
 messages = helper.create_message(data["image_frames"].flatten(0, 1))
 
 inputs = processor.apply_chat_template(
@@ -43,7 +22,7 @@ model_inputs = helper.to_device(model_inputs, "cuda")
 
 torch.cuda.manual_seed_all(42)
 
-start = time.time()
+# warmup
 with torch.autocast("cuda", dtype=torch.bfloat16):
     pred_xyz, pred_rot, extra = model.sample_trajectories_from_data_with_vlm_rollout(
         data=copy.deepcopy(model_inputs),
@@ -53,8 +32,23 @@ with torch.autocast("cuda", dtype=torch.bfloat16):
         max_generation_length=256,
         return_extra=True,
     )
+
+start = time.time()
+
+for _ in range(5):
+    with torch.autocast("cuda", dtype=torch.bfloat16):
+        pred_xyz, pred_rot, extra = model.sample_trajectories_from_data_with_vlm_rollout(
+            data=copy.deepcopy(model_inputs),
+            top_p=0.98,
+            temperature=0.6,
+            num_traj_samples=1,  # Feel free to raise this for more output trajectories and CoC traces.
+            max_generation_length=256,
+            return_extra=True,
+        )
 end = time.time()
+
+
 # the size is [batch_size, num_traj_sets, num_traj_samples]
 print("Chain-of-Causation (per trajectory):\n", extra["cot"][0])
 
-print(f"Latency: {end-start}")
+print(f"Avg latency: {(end-start)/5}")
